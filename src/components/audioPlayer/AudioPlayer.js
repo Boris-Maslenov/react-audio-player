bd901641c17400d931a0dc8a3e0e81ad06f208ec
Boris Maslenov <qwebot@gmail.com>
2023-06-04 14:23:03 +0300

    add AudioPlayer component

--- src/components/audioPlayer/AudioPlayer.js
+++ src/components/audioPlayer/AudioPlayer.js
@@ -1,8 +1,32 @@
+import './audioPlayer.scss';
+
 import React from 'react';
 import {useEffect, useState} from 'react';
 
 export const AudioPlayer = () => {
-    return 'AudioPlayer';
+    return (
+        <div class="audio-player-wrap">              
+            <div class="back-button">Back</div>
+            <div class="audio-player app-player__body">
+                <div class="audio-player__loading loading-animation">
+                    <span class="loading-animation__flash"></span>
+                </div>
+                <div class="audio-player__controls">
+                    <div class="audio-player__buttons">
+                        <button class="audio-player__button audio-player__play"></button>
+                        <button class="audio-player__button audio-player__pause"></button>
+                    </div>
+                    <div class="audio-player__progress">
+                        proggress
+                    </div>
+                    <div class="audio-player__bottom">
+                        <div class="audio-player__time">00:00</div>
+                        <div class="audio-player__volume">volume</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
  
--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 
 import { Form } from '../form';
+import { AudioPlayer } from '../audioPlayer/';
 
 export const Player = () => {
-    return <Form />
+    return(
+        <>
+           <Form />
+           <AudioPlayer />
+        </>
+     
+    ) 
 }
